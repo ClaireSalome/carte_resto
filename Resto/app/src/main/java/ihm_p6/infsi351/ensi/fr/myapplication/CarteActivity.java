0c7b213f42a5fe62e554b3b3192ca876bb104a08
ClaireSalome <claire.c.salome@gmail.com>
2017-04-17 20:59:13 +0200

    Carte : début sous-menus
    Quand on clique sur des sous menus comme "entrées froides" des plats
    s'affichent

--- Resto/app/src/main/java/ihm_p6/infsi351/ensi/fr/myapplication/CarteActivity.java
+++ Resto/app/src/main/java/ihm_p6/infsi351/ensi/fr/myapplication/CarteActivity.java
@@ -5,6 +5,7 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View ;
+import android.widget.Toast;
 
 /**
  * Created by lila on 07/04/17.
@@ -38,16 +39,79 @@ public class CarteActivity extends AppCompatActivity{
             idViewVisible = R.id.menu_pdj ;
         if(idBoutonMenu == R.id.entree)
             idViewVisible = R.id.menu_entree;
-        /*if(idBoutonMenu == R.id.plat)
+        if(idBoutonMenu == R.id.plat)
             idViewVisible = R.id.menu_plat;
         if(idBoutonMenu == R.id.dessert)
-            idViewVisible == R.id.menu_dessert;
+            idViewVisible = R.id.menu_dessert;
         if(idBoutonMenu == R.id.boisson)
-            idViewVisible = R.id.menu_boisson ;*/
+            idViewVisible = R.id.menu_boisson ;
 
         //afficher le menu
         findViewById(idViewVisible).setVisibility(View.VISIBLE);
         // grisé le bouton du menu sur lequel on est
         findViewById(idBoutonMenu).setBackgroundColor(Color.GRAY);
+    }
+
+
+    public void afficherSousMenuEntree(View v){
+        if (v.getId() == R.id.entree_f) {
+            if (findViewById(R.id.ssmenu_entreef).getVisibility() == View.GONE) {
+                findViewById(R.id.ssmenu_entreef).setVisibility(View.VISIBLE);
+                v.setBackground(getResources().getDrawable(R.drawable.up_arrow));
+            } else {
+                findViewById(R.id.ssmenu_entreef).setVisibility(View.GONE);
+                v.setBackground(getResources().getDrawable(R.drawable.down_arrow));
+            }
+        }
+        if (v.getId() == R.id.entree_c) {
+            if (findViewById(R.id.ssmenu_entreec).getVisibility() == View.GONE) {
+                findViewById(R.id.ssmenu_entreec).setVisibility(View.VISIBLE);
+                v.setBackground(getResources().getDrawable(R.drawable.up_arrow));
+            } else {
+                findViewById(R.id.ssmenu_entreec).setVisibility(View.GONE);
+                v.setBackground(getResources().getDrawable(R.drawable.down_arrow));
+            }
+        }
+    }
+
+
+    public void afficherSousMenuBoisson(View v){
+
+        if(v.getId() == R.id.eau) {
+            if (findViewById(R.id.ssmenu_eau).getVisibility() == View.GONE) {
+                findViewById(R.id.ssmenu_eau).setVisibility(View.VISIBLE);
+                v.setBackground(getResources().getDrawable(R.drawable.up_arrow));
+            } else {
+                findViewById(R.id.ssmenu_eau).setVisibility(View.GONE);
+                v.setBackground(getResources().getDrawable(R.drawable.down_arrow));
+            }
+        }
+        if(v.getId() == R.id.boisson_f){
+            if (findViewById(R.id.ssmenu_boisson_f).getVisibility() == View.GONE) {
+                findViewById(R.id.ssmenu_boisson_f).setVisibility(View.VISIBLE);
+                v.setBackground(getResources().getDrawable(R.drawable.up_arrow));
+            } else {
+                findViewById(R.id.ssmenu_boisson_f).setVisibility(View.GONE);
+                v.setBackground(getResources().getDrawable(R.drawable.down_arrow));
+            }
+        }
+        if(v.getId() == R.id.boisson_c){
+            if (findViewById(R.id.ssmenu_boisson_c).getVisibility() == View.GONE) {
+                findViewById(R.id.ssmenu_boisson_c).setVisibility(View.VISIBLE);
+                v.setBackground(getResources().getDrawable(R.drawable.up_arrow));
+            } else {
+                findViewById(R.id.ssmenu_boisson_c).setVisibility(View.GONE);
+                v.setBackground(getResources().getDrawable(R.drawable.down_arrow));
+            }
+        }
+        if(v.getId() == R.id.vin){
+            if (findViewById(R.id.ssmenu_vin).getVisibility() == View.GONE) {
+                findViewById(R.id.ssmenu_vin).setVisibility(View.VISIBLE);
+                v.setBackground(getResources().getDrawable(R.drawable.up_arrow));
+            } else {
+                findViewById(R.id.ssmenu_vin).setVisibility(View.GONE);
+                v.setBackground(getResources().getDrawable(R.drawable.down_arrow));
+            }
+        }
     }
 }
